de0b12e7a82f01a3ba91013d1b07bd5b898d03d4
James Sinkala <xinnks@gmail.com>
2023-01-25 21:35:24 +0300

    feat: Add login and register pages

---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Log in to your account!</h1>
+    <form>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" />
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" />
+        </div>
+        <div>
+            <button type="submit" >Submit</button>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+form{
+    display: flex;
+    flex-direction: column;
+    padding: 4px;
+}
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Create an account!</h1>
+    <form>
+        <div>
+            <label for="first-name">First Name</label>
+            <input type="text" id="first-name" />
+        </div>
+        <div>
+            <label for="surname">Surname</label>
+            <input type="text" id="surname" />
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" />
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" />
+        </div>
+        <div>
+            <label for="password">Repeat Password</label>
+            <input type="password" id="repeat-password" />
+        </div>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+</template>
