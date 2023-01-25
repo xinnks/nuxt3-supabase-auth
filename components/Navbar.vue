6c900a687d1a547d2d9f14945bae2829749aff9a
James Sinkala <xinnks@gmail.com>
2023-01-25 21:32:16 +0300

    feat: Add navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <div>
+            Log Out
+        </div>
+        <div>
+            Login
+        </div>
+        <div>
+            Register
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+nav{
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
