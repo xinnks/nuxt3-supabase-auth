798f3a58d3f6ddfcdbd2dbde218da11f9fcd98b7
James Sinkala <xinnks@gmail.com>
2023-01-28 00:04:59 +0300

    feat: Apply page route guards and authenticated session logic

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
+<script setup>
+const user = useSupabaseUser();
+const router = useRouter();
+definePageMeta({
+    middleware: 'auth'
+})
+watchEffect(async () => {
+    if(!user.value){
+        await router.push("/login");
+    }
+})
+</script>
+
 <template>
     <h1>Welcome to the dashboard!</h1>
 </template>
