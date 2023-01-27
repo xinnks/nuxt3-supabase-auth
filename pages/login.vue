5c8176b58cd878fb488992b1f920e7ca156bd2d0
James Sinkala <xinnks@gmail.com>
2023-01-27 19:42:01 +0300

    feat: Add account login logic

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,39 @@
+<script setup>
+const router = useRouter();
+const client = useSupabaseAuthClient();
+const user = useSupabaseUser();
+
+const credentials = reactive({
+    email: "",
+    password: ""
+});
+
+// Log into account
+async function login() {
+    const {email, password} = credentials;
+    const {error} = await client.auth.signInWithPassword({email, password});
+    if(!error) return router.push("/");
+    console.log(error)
+}
+
+// Watch if the user object is not null
+watchEffect(async () => {
+    if(user.value){
+        await router.push("/");
+    }
+})
+</script>
+
 <template>
     <h1>Log in to your account!</h1>
-    <form>
+    <form @submit.prevent="login()">
         <div>
             <label for="email">Email</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" v-model="credentials.email" />
         </div>
         <div>
             <label for="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" id="password" v-model="credentials.password" />
         </div>
         <div>
             <button type="submit" >Submit</button>
