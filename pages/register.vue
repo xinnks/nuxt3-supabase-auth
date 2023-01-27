1492a3bec6b9a0f7e993267254150f580bd193fc
James Sinkala <xinnks@gmail.com>
2023-01-27 20:03:19 +0300

    feat: Add user registration logic

--- pages/register.vue
+++ pages/register.vue
@@ -1,25 +1,59 @@
+<script setup>
+const client = useSupabaseAuthClient();
+
+const credentials = reactive({
+    firstName: "",
+    surname: "",
+    email: "",
+    password: "",
+    passwordRepeat: ""
+});
+
+async function register() {
+    const {firstName, surname, email, password, passwordRepeat} = credentials;
+    if(password !== passwordRepeat){
+        alert("Passwords don't match!");
+        return;
+    }
+    const {error} = await client.auth.signUp({
+        email, password,
+        options: {
+            data: {
+                first_name: firstName, surname, email
+            },
+            emailRedirectTo: "http://localhost:3000/login"
+        }
+    });
+    if (error){
+        alert('Something went wrong !');
+        return;
+    }
+    alert("Open the email we sent you to verify your account!");
+}
+</script>
+
 <template>
     <h1>Create an account!</h1>
-    <form>
+    <form @submit.prevent="register()">
         <div>
             <label for="first-name">First Name</label>
-            <input type="text" id="first-name" />
+            <input type="text" id="first-name" v-model="credentials.firstName" />
         </div>
         <div>
             <label for="surname">Surname</label>
-            <input type="text" id="surname" />
+            <input type="text" id="surname" v-model="credentials.surname" />
         </div>
         <div>
             <label for="email">Email</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" v-model="credentials.email" />
         </div>
         <div>
             <label for="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" id="password" v-model="credentials.password" />
         </div>
         <div>
             <label for="password">Repeat Password</label>
-            <input type="password" id="repeat-password" />
+            <input type="password" id="repeat-password" v-model="credentials.passwordRepeat" />
         </div>
         <div>
             <button type="submit">Submit</button>
